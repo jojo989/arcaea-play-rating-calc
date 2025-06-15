d64ee0fea9bbb47ff3a7dcc99cdb97b6f5e31178
jojo989 <70300454+jojo989@users.noreply.github.com>
2025-06-15 18:57:16 -0400

    Updates

---
+++ _app/immutable/nodes/0.C6sYmGW2.js
@@ -0,0 +1,1 @@
+import{c as n,a}from"../chunks/G6MPNSka.js";import{p}from"../chunks/B8MAPRTz.js";import{s}from"../chunks/CBvxUEsN.js";const i=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=p(e);s(t,()=>r.children),a(o,e)}export{u as component,f as universal};
